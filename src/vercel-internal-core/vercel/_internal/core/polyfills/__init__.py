02e6f2b3d5ecd15cf77b6ea6018bef762f54e7ed
Fantix King <fantix.king@gmail.com>
2026-09-02 12:42:02 -0400

    core: fix Buffer typing dependency

--- src/vercel-internal-core/vercel/_internal/core/polyfills/__init__.py
+++ src/vercel-internal-core/vercel/_internal/core/polyfills/__init__.py
@@ -4,6 +4,6 @@ Polyfills for modern Python features used by internal core.
 
 from ._datetime import UTC
 from ._strenum import StrEnum
-from ._typing import Self
+from ._typing import Buffer, Self
 
-__all__ = ("Self", "StrEnum", "UTC")
+__all__ = ("Buffer", "Self", "StrEnum", "UTC")
--- src/vercel-internal-core/vercel/_internal/core/polyfills/_typing.py
+++ src/vercel-internal-core/vercel/_internal/core/polyfills/_typing.py
@@ -1,8 +1,13 @@
 import sys
 
+if sys.version_info >= (3, 12):
+    from collections.abc import Buffer
+else:
+    from typing_extensions import Buffer
+
 if sys.version_info >= (3, 11):
     from typing import Self
 else:
     from typing_extensions import Self
 
-__all__ = ("Self",)
+__all__ = ("Buffer", "Self")
